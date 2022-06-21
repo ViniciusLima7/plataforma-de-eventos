20ae9d91f21a959888117b57831afaf194fadcd4
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-06-21 20:18:23 -0300

    Layout Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Lesson from "../Lesson";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6  border-l border-gray-600 ">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b  border-gray-500 block">
+        Cronograma de Aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+      </div>
+    </aside>
+  );
 }
