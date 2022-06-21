69ac933333b068c7b9135ae9531a62b288420ed4
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-06-21 00:58:32 -0300

    Mostrando Dados do Banco em Tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSON_QUERY);
+  console.log(data);
+
   return (
     <div>
       <h1 className="text-2xl text-center mt-9">Hello Dev</h1>
+      <ul>
+        {data?.lessons.map((lesson) => {
+          return <li key={lesson.id}>{lesson.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
