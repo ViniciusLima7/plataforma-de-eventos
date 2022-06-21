9c50f079c56d9e12a68ddb7c766527460d3c6e36
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-06-21 20:35:20 -0300

    Layout Lesson

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { CheckCircle } from "phosphor-react";
 
 export default function Lesson() {
-  return <div>Lesson</div>;
+  return (
+    <a href="">
+      <span className="text-gray-300">Terça • 22 de junho • 19h00</span>
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <CheckCircle size={20} />
+            Conteúdo liberado
+          </span>
+          <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold ">
+            AO VIVO
+          </span>
+        </header>
+        <strong className="text-gray-200 mt-5 block">
+          Abertura do evento Ignite Lab
+        </strong>
+      </div>
+    </a>
+  );
 }
