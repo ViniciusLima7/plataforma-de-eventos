ab579e31ab00db1a9d8204844975f06d91788bf8
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-06-22 20:54:03 -0300

    Mostra Video Dinamicamente

--- src/pages/Evento/index.tsx
+++ src/pages/Evento/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import Video from "../../components/Video";
 
 export default function Evento() {
+  const { slug } = useParams<{ slug: string }>();
+  console.log(slug);
   return (
     <div className="flex flex-col ">
       <Header />
       <main className=" flex flex-1 min-h-screen">
-        <Video />
+        {slug ? <Video /> : <div className="flex-1"></div>}
         <Sidebar />
       </main>
     </div>
