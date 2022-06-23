cb0e84ddda949ad190528c49bf1cff94369d9280
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-06-23 19:37:17 -0300

    Adiciona Estrutura da Pagina de Inscrição

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -2,13 +2,14 @@ import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { client } from "../../lib/apollo";
 import Evento from "../../pages/Evento";
+import Inscricao from "../../pages/Inscricao";
 
 export default function Router() {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>}></Route>
+          <Route path="/" element={<Inscricao />}></Route>
           <Route path="/event" element={<Evento />}></Route>
           <Route path="/event/lesson/:slug" element={<Evento />}></Route>
         </Routes>
